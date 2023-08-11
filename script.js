023ba4fd797abe46fb21cdf64358f3b09c3cdb7d
Elizabeth Bartholomew <lizzy.bartholomew@gmail.com>
2023-08-11 11:51:22 -0600

    updated

--- script.js
+++ script.js
@@ -3,25 +3,27 @@ function tipCalculator(){
     let total = document.getElementById("bill-amount").value;
     let people = document.getElementById("people-amount").value;
     let custom = document.getElementById("button-custom").value;
+    let percentage = 0;
 
     console.log(total);
     console.log(people);
 
     if ( total == "" || total == "0" ){    
-        // call total error function 
         document.getElementById("bill-amount").style.border = "2px solid red";
-        //document.getElementById("bill-amount").style.borderColor = "red";
+        document.getElementById("error-message-1").style.display = "block";
     }
 
     if ( people == "" || people == "0" ){    
-        // call people error function 
         document.getElementById("people-amount").style.border = "2px solid red";
+        document.getElementById("error-message-2").style.display = "block";
     }
+
+    
 
     let tipCalc = document.getElementById("tip-calc").innerHTML;
     let totalCalc = document.getElementById("total-calc").innerHTML;
 
-    let percentage = 0;
+    
 
     if (document.getElementById("5%").checked){
         percentage = 0.05;
@@ -43,6 +45,22 @@ function tipCalculator(){
         percentage = custom / 100;
     }
 
+    if (percentage == 0){
+        // error here
+        document.getElementById("error-message-3").style.display = "block";
+    } else{
+        document.getElementById("error-message-3").style.display = "none";
+    }
+
+    if ( total == "" || total == "0" || people == "" || people == "0"){
+        return
+    } else{
+        document.getElementById("bill-amount").style.border = "none";
+        document.getElementById("error-message-1").style.display = "none";
+        document.getElementById("people-amount").style.border = "none";
+        document.getElementById("error-message-2").style.display = "none";
+    }
+
     let number1 = 0;
     let number2 = 0;
 
@@ -53,3 +71,5 @@ function tipCalculator(){
     document.getElementById("total-calc").innerHTML = "$"+number2;
     
 }
+
+
